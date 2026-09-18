1e36d038c0c4ba27f251de68075c6168d886ff87
lin onetwo <linonetwo012@gmail.com>
2026-09-18 11:24:21 +0000

    refactor(web): establish query and app state layers

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import { I18nProvider } from "./i18n";
+import { queryClient } from "./state";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <I18nProvider><App /></I18nProvider>
+    <QueryClientProvider client={queryClient}>
+      <I18nProvider><App /></I18nProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
---
+++ web/src/state/index.ts
@@ -0,0 +1,2 @@
+export { queryClient } from "./queryClient";
+export { useAppStore, type AppTheme } from "./appStore";
